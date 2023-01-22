1dcab7a2598bdbe97ff2869c563729fcdfaa1418
Saurav Kumar Verma <sauravkumarverma22@gmail.com>
2023-01-22 11:39:18 +0530

    working on NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
+import {
+  AppBar,
+  IconButton,
+  Toolbar,
+  Drawer,
+  Button,
+  Avatar,
+  useMediaQuery,
+} from '@mui/material';
+
+import {
+  Brightness4,
+  Brightness7,
+  AccountCircle,
+  Menu,
+} from '@mui/icons-material';
+
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
+  // If window width > 600px , is Mobie will return false otherwise true
+  const isMobile = useMediaQuery('(max-width:600px');
   return (
-    <div>
-      <h1>This is NavBar</h1>
-    </div>
+    <>
+      <AppBar position="fixed">
+        <Toolbar className="nav-toolbar">
+          {isMobile && (
+            <IconButton
+              color="inherit"
+              edge="start"
+              sx={{
+                outline: 'none',
+                marginRight: 'theme.spacing(2)',
+              }}
+              onClick={() => {}}
+            >
+              <Menu />
+            </IconButton>
+          )}
+        </Toolbar>
+      </AppBar>
+    </>
   );
 };
 
